04e7a7f94fdd474daf3565733b7e062ab1ffa10b
llxgg <491894345@qq.com>
2021-02-13 09:23:20 +0800

    自动化部署测试20

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <div style="font-size: 30px; color:red;">测试自动化部署~~~~</div>
-    <div style="font-size: 30px; color:red;">nice~~~~整套自动化终于部署成功了！</div>
+    <div style="font-size: 30px; color:red;">nice~~~~</div>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
